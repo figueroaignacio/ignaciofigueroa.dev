ac9b01ba4d05ef1ee14d61037a0584f19da918e5
figueroaignacio <ignaciofigueroadev@gmail.com>
2026-06-01 15:38:00 -0300

    feat: add subtle background grid decorations to main layout

--- src/app/[locale]/(main)/layout.tsx
+++ src/app/[locale]/(main)/layout.tsx
@@ -1,5 +1,6 @@
 import { Dock } from '@/shared/components/dock';
 import { Footer } from '@/shared/components/footer';
+import { BackgroundDecorations } from '@/shared/components/background-decorations';
 import { routing } from '@/i18n/routing';
 import { hasLocale, type Locale } from 'next-intl';
 import { setRequestLocale } from 'next-intl/server';
@@ -19,13 +20,14 @@ export default async function MainLayout({ children, params }: LocaleLayoutProps
   setRequestLocale(locale);
 
   return (
-    <div className="min-h-screen flex flex-col">
-      <main id="main-content" className="flex-1 container" tabIndex={-1}>
+    <div className="min-h-screen flex flex-col relative overflow-x-hidden">
+      <BackgroundDecorations />
+      <main id="main-content" className="flex-1 container relative z-10" tabIndex={-1}>
         {children}
       </main>
       <Footer />
       {/* Safe area spacer for fixed dock */}
-      <div className="h-32 w-full shrink-0" aria-hidden="true" />
+      <div className="h-32 w-full shrink-0 relative z-10" aria-hidden="true" />
       <Dock />
     </div>
   );
